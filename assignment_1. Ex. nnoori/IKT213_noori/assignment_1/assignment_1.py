107a21af0328a20f1b7acd4b367a5c545ea7c00c
berke <daw.berkeij@student.han.nl>
2025-08-18 13:53:20 +0200

    remove duplicate

--- assignment_1. Ex. nnoori/IKT213_noori/assignment_1/assignment_1.py
+++ assignment_1. Ex. nnoori/IKT213_noori/assignment_1/assignment_1.py
@@ -11,12 +11,12 @@ def print_image_information(image):
     Print the information of the given image.
     """
     print("Image Information:")
-    print(f"    Height: {image.shape[0]}")
-    print(f"    Width: {image.shape[1]}")
-    print(f"    Channels: {image.shape[2]}")
-    print(f"    Image Size: {image.size} bytes")
-    print(f"    Image Data Type: {image.dtype}")
+    print(f"\tHeight: {image.shape[0]}")
+    print(f"\tWidth: {image.shape[1]}")
+    print(f"\tChannels: {image.shape[2]}")
+    print(f"\tImage Size: {image.size} bytes")
+    print(f"\tImage Data Type: {image.dtype}")
 
 if __name__ == "__main__":
-    image = cv2.imread("assignment_1. Ex. nnoori\IKT213_noori\lena-1.png")
+    image = cv2.imread("assignment_1. Ex. nnoori/IKT213_noori/assignment_1/lena-1.png")
     print_image_information(image)
