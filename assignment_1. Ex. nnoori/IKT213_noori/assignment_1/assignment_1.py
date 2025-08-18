f2c54835eaed51c1aacf2cd80eb842e3ae9a25cb
berke <daw.berkeij@student.han.nl>
2025-08-18 14:10:07 +0200

    finished assinment 1

--- assignment_1. Ex. nnoori/IKT213_noori/assignment_1/assignment_1.py
+++ assignment_1. Ex. nnoori/IKT213_noori/assignment_1/assignment_1.py
@@ -17,6 +17,22 @@ def print_image_information(image):
     print(f"\tImage Size: {image.size} bytes")
     print(f"\tImage Data Type: {image.dtype}")
 
+def get_camera_outputs():
+    """
+    saves the fps, height, and width from the webcam
+    """
+    cap = cv2.VideoCapture(0)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    cap.release()
+
+    with open("assignment_1. Ex. nnoori/IKT213_noori/assignment_1/solutions/camera_outputs.txt", "w+") as f:
+        f.write(f"FPS: {fps}\n")
+        f.write(f"Width: {width}\n")
+        f.write(f"Height: {height}\n")
+
 if __name__ == "__main__":
     image = cv2.imread("assignment_1. Ex. nnoori/IKT213_noori/assignment_1/lena-1.png")
     print_image_information(image)
+    get_camera_outputs()
