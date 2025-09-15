38bf5aa4072b7f450b2ab67038981c726c4d13d0
berke <daw.berkeij@student.han.nl>
2025-09-15 11:16:18 +0200

    made it PEP8

--- assignment_3/assignment_3.py
+++ assignment_3/assignment_3.py
@@ -6,6 +6,7 @@ Date: 15-09-2025
 import cv2
 import numpy as np
 
+
 def detect_edges_sobel(image):
     """
     Detect edges in an image using the Sobel operator.
@@ -22,6 +23,7 @@ def detect_edges_sobel(image):
 
     return sobelxy
 
+
 def detect_edge_canny(image):
     """
     Detect edges in an image using the Canny edge detector.
@@ -37,6 +39,7 @@ def detect_edge_canny(image):
     edges = cv2.Canny(bimg_blur, 50, 50)
 
     return edges
+
 
 def match_template(image, template):
     """
@@ -55,10 +58,11 @@ def match_template(image, template):
     loc = np.where(result >= threshold)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(image, pt, (pt[0] + template.shape[1], pt[1] + 
+        cv2.rectangle(image, pt, (pt[0] + template.shape[1], pt[1] +
                                   template.shape[0]), (0, 0, 255), 2)
     new_image = image
     return new_image
+
 
 def resize_image(image, scale_factor: int, up_or_down: str):
     """
@@ -76,7 +80,7 @@ def resize_image(image, scale_factor: int, up_or_down: str):
     rows, cols = image.shape[:2]
 
     if up_or_down == 'up':
-        resized_image = cv2.pyrUp(image, dstsize=(scale_factor * cols, 
+        resized_image = cv2.pyrUp(image, dstsize=(scale_factor * cols,
                                                   scale_factor * rows))
     elif up_or_down == 'down':
         resized_image = cv2.pyrDown(image, dstsize=(
@@ -88,29 +92,29 @@ def resize_image(image, scale_factor: int, up_or_down: str):
 
 
 if __name__ == "__main__":
-    image_path_lambo = 'assignment_3/lambo.png'
-    image_lambo = cv2.imread(image_path_lambo, cv2.IMREAD_GRAYSCALE)
+    IMAGE_PATH_LAMBO = 'assignment_3/lambo.png'
+    image_lambo = cv2.imread(IMAGE_PATH_LAMBO, cv2.IMREAD_GRAYSCALE)
     edges_sobel = detect_edges_sobel(image_lambo)
     edges_canny = detect_edge_canny(image_lambo)
 
-    image_path_shapes = 'assignment_3/shapes-1.png'
-    image_path_shapes_template = 'assignment_3/shapes_template.jpg'
+    IMAGE_PATH_SHAPES = 'assignment_3/shapes-1.png'
+    IMAGE_PATH_SHAPES_TEMPLATE = 'assignment_3/shapes_template.jpg'
 
-    image_shapes = cv2.imread(image_path_shapes, cv2.IMREAD_GRAYSCALE)
-    image_shapes_template = cv2.imread(image_path_shapes_template, 
+    image_shapes = cv2.imread(IMAGE_PATH_SHAPES, cv2.IMREAD_GRAYSCALE)
+    image_shapes_template = cv2.imread(IMAGE_PATH_SHAPES_TEMPLATE,
                                        cv2.IMREAD_GRAYSCALE)
     matched_image = match_template(image_shapes, image_shapes_template)
 
     resized_image_up = resize_image(image_lambo, 2, 'up')
     resized_image_down = resize_image(image_lambo, 2, 'down')
     # save images
-    cv2.imwrite('assignment_3/solutions/edges_sobel.png', 
+    cv2.imwrite('assignment_3/solutions/edges_sobel.png',
                 edges_sobel)
-    cv2.imwrite('assignment_3/solutions/edges_canny.png', 
+    cv2.imwrite('assignment_3/solutions/edges_canny.png',
                 edges_canny)
-    cv2.imwrite('assignment_3/solutions/matched_image.png', 
+    cv2.imwrite('assignment_3/solutions/matched_image.png',
                 matched_image)
-    cv2.imwrite('assignment_3/solutions/resized_image_up.png', 
+    cv2.imwrite('assignment_3/solutions/resized_image_up.png',
                 resized_image_up)
-    cv2.imwrite('assignment_3/solutions/resized_image_down.png', 
+    cv2.imwrite('assignment_3/solutions/resized_image_down.png',
                 resized_image_down)
